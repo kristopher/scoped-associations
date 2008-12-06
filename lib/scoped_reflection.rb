7da2457597a11b26bd0ec76a2d6d8227beda8a4e
Kris <igg@six.(none)>
2008-12-05 19:35:19 -0500

    Removing extra end. fixing indentation.

--- lib/scoped_reflection.rb
+++ lib/scoped_reflection.rb
@@ -6,24 +6,23 @@ module ScopedReflection
 
   def scope_reflection(reflection)
 
-      source_reflection_class = 
-        if reflection.is_a? ActiveRecord::Reflection::ThroughReflection 
-          reflection.source_reflection.klass 
-        else 
-          reflection.klass
-        end
-      
-      scope_names = [reflection.options[:scope]].flatten
-      final_scope = scope_names.inject(source_reflection_class) do |accumulated_scope, scope|
-        if accumulated_scope.respond_to?(scope)
-          accumulated_scope.send(scope)
-        else
-          raise ActiveRecord::UndefinedScopeReflectionError.new(self, scope)
-        end
+    source_reflection_class = 
+      if reflection.is_a? ActiveRecord::Reflection::ThroughReflection 
+        reflection.source_reflection.klass 
+      else 
+        reflection.klass
       end
-
-      current_scoped_methods = final_scope.send(:current_scoped_methods) || {}
-      reflection.options.merge!(current_scoped_methods[:find] || {}) 
+    
+    scope_names = [reflection.options[:scope]].flatten
+    final_scope = scope_names.inject(source_reflection_class) do |accumulated_scope, scope|
+      if accumulated_scope.respond_to?(scope)
+        accumulated_scope.send(scope)
+      else
+        raise ActiveRecord::UndefinedScopeReflectionError.new(self, scope)
+      end
     end
+
+    current_scoped_methods = final_scope.send(:current_scoped_methods) || {}
+    reflection.options.merge!(current_scoped_methods[:find] || {}) 
   end
 end
