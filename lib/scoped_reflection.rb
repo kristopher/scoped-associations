e911bebb8bad0ae2ea7b7b3e037bcc251401c2ed
Elijah Miller <elijah.miller@gmail.com>
2008-12-05 19:28:39 -0500

    Breaking up some long lines.

--- lib/scoped_reflection.rb
+++ lib/scoped_reflection.rb
@@ -6,7 +6,6 @@ module ScopedReflection
 
   def scope_reflection(reflection)
     if reflection.options && reflection.options[:scope]
-      
       source_reflection_class = 
         if reflection.options[:through] && reflection.respond_to?(:source_reflection) 
           reflection.source_reflection.klass 
@@ -14,14 +13,17 @@ module ScopedReflection
           reflection.klass
         end
       
-      final_scope = [reflection.options[:scope]].flatten.inject(source_reflection_class) do |accumulated_scope, scope|
+      scope_names = [reflection.options[:scope]].flatten
+      final_scope = scope_names.inject(source_reflection_class) do |accumulated_scope, scope|
         if accumulated_scope.respond_to?(scope)
           accumulated_scope.send(scope)
         else
           raise ActiveRecord::UndefinedScopeReflectionError.new(self, scope)
         end
       end
-      reflection.options.merge!((final_scope.send(:current_scoped_methods) || {})[:find] || {}) 
+
+      current_scoped_methods = final_scope.send(:current_scoped_methods) || {}
+      reflection.options.merge!(current_scoped_methods[:find] || {}) 
     end
   end
 end
