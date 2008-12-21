cdcc5a460401b4e1452f6cd29e60cd3b879596be
Elijah Miller <elijah.miller@gmail.com>
2008-12-21 18:29:42 -0500

    Changing order of specs to show an issue related to condition building.

--- spec/scoped_associations_spec.rb
+++ spec/scoped_associations_spec.rb
@@ -6,63 +6,62 @@ describe "ScopedAssociation" do
   end
   
   describe "standard assocations" do
-    describe "has_many" do
-      it "should return all children" do
-        @parent.children.collect(&:name).should == ['aa', 'ab']
-      end
-    end
-
     describe "has_many :through" do
       it "should return all grand children" do
         @parent.grand_children.collect(&:name).should == ['aaa', 'aab', 'aba', 'abb']
+      end
+    end
+
+    describe "has_many" do
+      it "should return all children" do
+        @parent.children.collect(&:name).should == ['aa', 'ab']
       end
     end
   end
 
 
   describe "scope on direct assocation" do
-    describe "has_many" do
-      it "should should return all live children" do
-        @parent.children_direct.collect(&:name).should == ['aa']
-      end
-    end
-
     describe "has_many :through" do
       it "should should return all grand children with a live parent" do
         @parent.grand_children_direct.collect(&:name).should == ['aaa', 'aab']
+      end
+    end
+
+    describe "has_many" do
+      it "should should return all live children" do
+        @parent.children_direct.collect(&:name).should == ['aa']
       end
     end
   end
 
 
   describe "scope on indrect assocation" do
-    describe "has_many" do
-      it "should return all children" do
-        @parent.children_indirect.collect(&:name).should == ['aa', 'ab']
-      end
-    end
-
     describe "has_many :through" do
       it "should should return all live grand children" do
         @parent.grand_children_indirect.collect(&:name).should == ['aaa', 'aba']
+      end
+    end
+
+    describe "has_many" do
+      it "should return all children" do
+        @parent.children_indirect.collect(&:name).should == ['aa', 'ab']
       end
     end
   end
 
 
   describe "scope on direct and indrect assocation" do
-    describe "has_many" do
-      it "should return all live children" do
-        @parent.children_direct_and_indirect.collect(&:name).should == ['aa']
-      end
-    end
-
     describe "has_many :through" do
       it "should return all live grandchildren with a live parent" do
         @parent.grand_children_direct_and_indirect.collect(&:name).should == ['aaa']
       end
     end
-  end
 
+    describe "has_many" do
+      it "should return all live children" do
+        @parent.children_direct_and_indirect.collect(&:name).should == ['aa']
+      end
+    end
+  end
 
 end
